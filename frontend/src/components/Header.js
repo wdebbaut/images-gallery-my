caaa51e9f42f14283cda7e810af498db80858150
wdebbaut <debbaut.wim@gmail.com>
2023-02-22 16:40:54 +0100

    added search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
-const Header = (props) => {
-    const {title} = props;
-    return  (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-          </Navbar>
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+};
+
+const Header = ({ title }) => {
+    return (
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
+        </Navbar>
     )
-}
+};
 
 export default Header;
