b39dc761712ecae638a578a371edc20b57df7c52
wdebbaut <debbaut.wim@gmail.com>
2023-02-22 16:13:00 +0100

    change again app and header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
 const Header = (props) => {
-    const{ title} = props;
+    const {title} = props;
     return  (
         <Navbar bg="light" variant="light">
             <Navbar.Brand href="/">{title}</Navbar.Brand>
