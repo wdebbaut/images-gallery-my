776885a99ef3c95904fa91b19514052002bedeee
wdebbaut <debbaut.wim@gmail.com>
2023-02-23 15:58:40 +0100

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,15 +19,14 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
       });
-    setWord("");
-  }
+    setWord('');
+  };
 
-  
   return (
     <div>
       <Header title="Images Gallery" />
